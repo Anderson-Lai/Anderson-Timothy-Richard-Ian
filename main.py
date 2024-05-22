2796d0996d51d91586155fd58aa3cb358bdfb879
Anderson-Lai <andersonlai@DESKTOP-KI6FV0O>
2024-05-22 13:33:49 -0400

    generated boilerpalte

---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+
+
+def main():
+    
+    # pygame template
+    pygame.init()
+    
+    WIDTH = 640
+    HEIGHT = 480
+    SIZE = (WIDTH, HEIGHT)
+    
+    screen = pygame.display.set_mode(SIZE)
+    clock = pygame.time.Clock()
+    
+    # ---------------------------
+    # Initialize global variables
+    
+    
+    
+    # ---------------------------
+    
+    running = True
+    while running:
+        # EVENT HANDLING
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+    
+        # GAME STATE UPDATES
+        # All game math and comparisons happen here
+    
+        # DRAWING
+        screen.fill((255, 255, 255))  # always the first drawing command
+        
+        
+        
+        
+        
+        # Must be the last two lines
+        # of the game loop
+        pygame.display.flip()
+        clock.tick(30)
+        #---------------------------
+    
+    pygame.quit()
+    
+    return 0
+
+if __name__ == "__main__":
+    main()
