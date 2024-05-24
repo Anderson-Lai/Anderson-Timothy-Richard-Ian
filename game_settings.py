3a68add20462e64bf11e6a66bd3bd9dec4fb46bb
Anderson-Lai <andersonlai@DESKTOP-KI6FV0O>
2024-05-24 14:25:57 -0400

    refactoring

--- game_settings.py
+++ game_settings.py
@@ -4,22 +4,7 @@ import json
 def game_settings(screen, difficulty: int, sensitivity: int):
     screen.fill((0, 0, 0))
 
-    # sensitivy and difficulty button
-    difficulties = ["easy", "normal", "hard", "impossible"]
-
-    settings = {
-        "difficulty": difficulties[difficulty],
-        "sensitivity": sensitivity,
-    }
-    
-    # opens file and truncates it
-    file = open("settings.txt", "w")
-    # write the settings to the file
-    json.dump(settings, file)
-
-    # cleanup
-    file.close()
-
+    # sensitivy and difficulty buttons
     # difficulty
     pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(300, 200, 200, 100))
 
@@ -32,3 +17,16 @@ def game_settings(screen, difficulty: int, sensitivity: int):
 
     # go back to main menu
     pygame.draw.rect(screen, (144, 238, 144), pygame.Rect(50, 50, 125, 125))
+
+    difficulties = ["easy", "normal", "hard", "impossible"]
+    settings = {
+        "difficulty": difficulties[difficulty],
+        "sensitivity": sensitivity,
+    }
+    
+    # opens file and truncates it
+    file = open("settings.txt", "w")
+    # write the settings to the file
+    json.dump(settings, file)
+    # cleanup
+    file.close()
