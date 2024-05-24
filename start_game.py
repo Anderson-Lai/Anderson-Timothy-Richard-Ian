ee326c75395b5abbb092ce2c42dfd656a6adebd3
Richard Li <159478753+eggboiobgge@users.noreply.github.com>
2024-05-23 22:06:02 -0400

    added spaceship unit

--- start_game.py
+++ start_game.py
@@ -1,5 +1,7 @@
 import pygame
 
-def start_game(screen):
-    
+def start_game(screen, location):
     screen.fill((0, 255, 255))
+    pygame.draw.rect(screen, (0, 255, 0), (location, 770, 50, 50))
+
+    
