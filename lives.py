d872f62c7ecbebb560e0df0e77d7e59cd5b3a100
timothywuu <147197375+timothywuu@users.noreply.github.com>
2024-05-24 14:27:36 -0400

    Made 2 functions to draw and remove player lives based on difficulty

---
+++ lives.py
@@ -0,0 +1,19 @@
+def numHearts(gameDifficulty:str) -> int:
+    hardLives = 1
+    mediumLives = 3
+    easyLives = 5
+    if gameDifficulty == "easy":
+        lives = easyLives
+    elif gameDifficulty == "medium":
+        lives = mediumLives
+    elif gameDifficulty == "hard":
+        lives = hardLives
+    return lives
+
+def draw_removeHearts(hearts:int, hit:bool):
+    heartColour = (255, 0, 0)
+    heartRadius = 20
+    if hit == True:
+        hearts -= 1
+    for i in range(hearts):
+        pygame.draw.circle(screen, heartColour, ((WIDTH-heartRadius*2.5)-(heartRadius*3)*i, heartRadius*2.5), heartRadius)
