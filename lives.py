985c8c99b95491137730e8bd69c3d9036196055b
timothywuu <147197375+timothywuu@users.noreply.github.com>
2024-05-24 14:29:49 -0400

    added some comments

--- lives.py
+++ lives.py
@@ -17,3 +17,13 @@ def draw_removeHearts(hearts:int, hit:bool):
         hearts -= 1
     for i in range(hearts):
         pygame.draw.circle(screen, heartColour, ((WIDTH-heartRadius*2.5)-(heartRadius*3)*i, heartRadius*2.5), heartRadius)
+
+
+'''
+These are the variables that make the functions run
+gameDifficulty = "easy"
+hearts = numHearts(gameDifficulty)
+hit = True
+'''
+
+# this should go under GAME UPDATES. draw_removeHearts(hearts, hit)
