0218ea108a8662bf7267810487a6cc9cbdaea573
Richard Li <159478753+eggboiobgge@users.noreply.github.com>
2024-05-24 14:56:25 -0400

    add in game menu

--- start_game.py
+++ start_game.py
@@ -3,5 +3,5 @@ import pygame
 def start_game(screen, location):
     screen.fill((0, 255, 255))
     pygame.draw.rect(screen, (0, 255, 0), (location, 770, 50, 50))
-
+    pygame.draw.rect(screen, (0, 0, 0),(600, 0, 200, 800))
     
