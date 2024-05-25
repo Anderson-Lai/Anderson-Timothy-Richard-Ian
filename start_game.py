90e9e65f10a02de75d1539681ba9fc80a7a2fd92
Richard Li <159478753+eggboiobgge@users.noreply.github.com>
2024-05-25 13:35:16 -0400

    Added border so it looks better

--- start_game.py
+++ start_game.py
@@ -2,6 +2,8 @@ import pygame
 
 def start_game(screen, location):
     screen.fill((0, 255, 255))
-    pygame.draw.rect(screen, (0, 255, 0), (location, 770, 50, 50))
-    pygame.draw.rect(screen, (0, 0, 0),(600, 0, 200, 800))
-    
+    pygame.draw.rect(screen, (0, 255, 0), (location + 5, 720, 50, 50))
+    pygame.draw.rect(screen, (0, 0, 0), (600, 0, 200, 800))
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 20, 800))
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 600, 20))
+    pygame.draw.rect(screen, (0, 0, 0), (0, 780, 600, 20))
