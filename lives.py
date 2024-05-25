97a64b3c24bc0cc9c9597bd35da6d4929e36f7f1
timothywuu <147197375+timothywuu@users.noreply.github.com>
2024-05-24 21:38:52 -0400

    changed some variable wording to make more sense

--- lives.py
+++ lives.py
@@ -1,4 +1,4 @@
-def numHearts(gameDifficulty:str) -> int:
+def numLives(gameDifficulty:str) -> int:
     easyLives = 5
     mediumLives = 3
     hardLives = 1
@@ -9,20 +9,20 @@ def numHearts(gameDifficulty:str) -> int:
     elif gameDifficulty == "hard":
         return hardLives
 
-def draw_removeHearts(hearts:int, hit:bool):
+def draw_removeHearts(lives:int, hit:bool):
     heartColour = (255, 0, 0)
     heartRadius = 20
     if hit == True:
-        hearts -= 1
-    for i in range(hearts):
+        lives -= 1
+    for i in range(lives):
         pygame.draw.circle(screen, heartColour, ((WIDTH-heartRadius*2.5)-(heartRadius*3)*i, heartRadius*2.5), heartRadius)
 
 
 '''
 These are the variables that make the functions run
 gameDifficulty = "easy"
-hearts = numHearts(gameDifficulty)
+lives = numHearts(gameDifficulty)
 hit = True
 '''
 
-# this should go under GAME UPDATES. draw_removeHearts(hearts, hit)
+# this should go under GAME UPDATES. draw_removeHearts(lives, hit)
