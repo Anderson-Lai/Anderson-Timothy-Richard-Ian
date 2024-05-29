70657f4670c660081f770cbb5cf59597589b5660
Anderson-Lai <andersonmhlai26@gmail.com>
2024-05-29 12:42:32 -0400

    refactored score file functions

--- gameResults/save_score.py
+++ gameResults/save_score.py
@@ -1,5 +1,6 @@
 import json
 from gameResults.get_high_score import get_high_score
+from gameResults.get_score_file import get_score_file
 
 def save_score(score: int) -> None:
 
@@ -7,10 +8,10 @@ def save_score(score: int) -> None:
     if score >= high_score:
         high_score = score
 
-    json_score = {
-        "highScore": high_score,
-        "currentScore": score,
-    }
+    score_file = get_score_file()
+
+    score_file["highScore"] = high_score
+    score_file["currentScore"] = score
 
     with open("score.json", "w") as file:
-        json.dump(json_score, file)
+        json.dump(score_file, file)
