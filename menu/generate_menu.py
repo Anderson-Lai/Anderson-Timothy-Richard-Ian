175d241bc16e3352d43aa9d0b803a6069554c9e6
timothywuu <147197375+timothywuu@users.noreply.github.com>
2024-05-30 13:40:31 -0400

    added 'play' text to start game button

--- menu/generate_menu.py
+++ menu/generate_menu.py
@@ -17,7 +17,14 @@ def generate_menu(screen):
     pygame.draw.rect(screen, (211, 211, 211), pygame.Rect(650, 575, 100, 100))
     settings_icon = pygame.image.load("settings_icon.png")
     smaller_settings_icon = pygame.transform.scale(settings_icon, (85, 85))
-    screen.blit(settings_icon_small, (659, 582.5))
+    screen.blit(settings_icon_small, (658.25, 582.5))
 
     # start game button
     pygame.draw.rect(screen, (255, 255, 237), pygame.Rect(175, 550, 450, 150))
+    play_font_type = "sfnsmono"
+    play_font_size = 65
+    play_font_bold = True
+    play_font_colour = (0, 0, 0)
+    play_font = pygame.font.SysFont(play_font_type, play_font_size, play_font_bold)
+    play_game_text = play_font.render("PLAY", True, play_font_colour)
+    screen.blit(play_game_text, (320, 582.5))
