a9cf2d3902e0e20c2191b68fa01583e95ea417c5
timothywuu <147197375+timothywuu@users.noreply.github.com>
2024-05-30 13:30:26 -0400

    added settings icon to settings button

--- menu/generate_menu.py
+++ menu/generate_menu.py
@@ -15,6 +15,9 @@ def generate_menu(screen):
 
     # open settings
     pygame.draw.rect(screen, (211, 211, 211), pygame.Rect(650, 575, 100, 100))
+    settings_icon = pygame.image.load("settings_icon.png")
+    smaller_settings_icon = pygame.transform.scale(settings_icon, (85, 85))
+    screen.blit(settings_icon_small, (659, 582.5))
 
     # start game button
     pygame.draw.rect(screen, (255, 255, 237), pygame.Rect(175, 550, 450, 150))
