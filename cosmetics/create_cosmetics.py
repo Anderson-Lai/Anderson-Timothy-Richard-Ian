97856fda4270bb43e3fed2faefd9564d5de32ca8
Anderson-Lai <andersonmhlai26@gmail.com>
2024-05-31 13:07:40 -0400

    fixed bugs with cosmetic

--- cosmetics/create_cosmetics.py
+++ cosmetics/create_cosmetics.py
@@ -4,23 +4,7 @@ def create_cosmetics() -> None:
     
     json_cosmetics = {
         # colours
-        "red": False,
-        "orange": False,
-        "yellow": False,
-        "green": False,
-        "blue": False,
-        "purple": False,
-        "white": False,
-        "black": False,
-        "rainbow": False,
-        "pink": False,
-        "brown": False,
-        "aqua": False,
-        "gray": False,
-
-        # skins
-        "leo": False,
-        "eric": False,
+        "shipColour": "green",
     }
 
     try:
