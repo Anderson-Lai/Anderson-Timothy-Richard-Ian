1822c3b48fc29de4d7e8d78e862996cdc9246681
Anderson-Lai <andersonmhlai26@gmail.com>
2024-06-13 10:13:09 -0400

    fixed more spelling errors

--- modifications/changing/change_coins.py
+++ modifications/changing/change_coins.py
@@ -5,9 +5,9 @@ def change_coins(amount: int) -> None:
     coins = get_coins()
     coins += amount
     
-    with open("./jsonFiles/modifcations.json", "r") as file:
+    with open("./jsonFiles/modifications.json", "r") as file:
         settings = json.load(file)
         settings["coins"] = coins
     
-    with open("./jsonFiles/modifcations.json", "w") as file:
+    with open("./jsonFiles/modifications.json", "w") as file:
         json.dump(settings, file)
