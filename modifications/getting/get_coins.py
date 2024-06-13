d5702d97b257cd956e4a3e543992f86102face1a
Anderson-Lai <andersonmhlai26@gmail.com>
2024-06-13 10:12:32 -0400

    fixed spelling error in json

--- modifications/getting/get_coins.py
+++ modifications/getting/get_coins.py
@@ -2,6 +2,6 @@ import json
 
 
 def get_coins() -> int:
-    with open("./jsonFiles/modifcations.json", "r") as file:
+    with open("./jsonFiles/modifications.json", "r") as file:
         settings = json.load(file)
         return settings["coins"]
